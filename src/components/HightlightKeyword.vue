73d8ffbc0f3271fa0e8576dbc45bd46217ee0f10
xiaofeihe <1712776213@qq.com>
2021-08-07 19:41:05 +0800

    fix: 关键词高亮

--- src/components/HightlightKeyword.vue
+++ src/components/HightlightKeyword.vue
@@ -1,8 +1,10 @@
 <template>
-  <div class="heightlight-keyword">dddd</div>
+  <div class="heightlight-keyword" v-html="result">
+  </div>
 </template>
 
 <script>
+import { reactive, toRefs, watch } from 'vue'
 export default {
   name: 'HeightlightKeyword',
   props: {
@@ -11,8 +13,21 @@ export default {
       default: ''
     }
   },
-  setup () {
-    console.log('...')
+  setup (props) {
+    const state = reactive({
+      result: '',
+      keywords: ['加快', '加强', '加大', '支持', '推进', '推动', '增强', '加强']
+    })
+    watch(
+      () => props.article,
+      () => {
+        state.result = props.article
+        for (let i = 0; i < state.keywords.length; i++) {
+          state.result = state.result.replaceAll(state.keywords[i], `<span style="background-color: yellow;">${state.keywords[i]}</span>`)
+        }
+      }
+    )
+    return { ...toRefs(state) }
   }
 }
 </script>
@@ -20,5 +35,12 @@ export default {
 <style lang="less" scoped>
 .heightlight-keyword {
   width: 100%;
+  height: 800px;
+  white-space: pre-line;
+  text-align: left;
+  overflow-y: auto;
+}
+.heightlight-keyword::-webkit-scrollbar {
+  display: none;
 }
 </style>
